b65c49dd6e0c774d684addeb1c9af7268e400518
Sachith Nimendra <126912947+sachithnim@users.noreply.github.com>
2024-06-19 01:26:10 +0530

    Create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -15,10 +16,13 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
     });
 
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
