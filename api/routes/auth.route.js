b140ec3fb647f512d8b8a84b111936e1fbcd937b
sachithnim <nimendras07@gmail.com>
2024-07-05 23:10:42 +0530

    Fixed error

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -7,6 +7,6 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.post('/google', google)
+router.post('/google', google);
 
 export default router;
