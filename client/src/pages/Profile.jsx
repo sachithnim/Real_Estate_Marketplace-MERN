9d2acc221166a87e34140b5d7177626d1f6ad9c2
sachithnim <nimendras07@gmail.com>
2024-07-07 23:28:43 +0530

    Add image upload function when click image

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,17 @@
 import { useSelector } from 'react-redux';
+import { useRef } from 'react';
 
 export default function Profile() {
+    const fileRef = useRef(null);
     const { currentUser } = useSelector((state) => state.user);
 
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
             <form className="flex flex-col gap-4">
-                <input type='file'></input>
+                <input type='file' ref={fileRef} hidden></input>
                 <img 
+                    onClick={() => fileRef.current.click()}
                     src={currentUser.avatar} 
                     alt="profile" 
                     className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
