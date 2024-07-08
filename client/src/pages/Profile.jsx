7f43489ab37f9d66ba7a90c83aba22f97879542f
sachithnim <nimendras07@gmail.com>
2024-07-08 23:37:17 +0530

    image

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,45 @@
 import { useSelector } from 'react-redux';
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import {getStorage, ref} from 'firebase/storage';
+import {app} from '../firebase';
 
 export default function Profile() {
     const fileRef = useRef(null);
     const { currentUser } = useSelector((state) => state.user);
+    const [file, setFile] = useState(undefined);
+    console.log(file);
 
+    //firebase storage
+
+    // allow read;
+    // allow write : if
+    // request.resource.size < 2 * 1024 * 1024 &&
+    // request.resource.contentType.matches('image/.*')
+
+    useEffect(() => {
+        if(file) {
+            handleFileUpload(file);
+        }
+    }, [file]);    
+
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+            }
+        )
+    }
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
             <form className="flex flex-col gap-4">
-                <input type='file' ref={fileRef} hidden></input>
+                <input onChange={(e) => setFile(e.target.files[0])}  type='file' ref={fileRef} hidden accept='image/*'></input>
                 <img 
                     onClick={() => fileRef.current.click()}
                     src={currentUser.avatar} 
